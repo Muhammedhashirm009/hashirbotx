902980f7ff56f34154ca6d2421c1969dded1dd41
Mikhaiel_Offical <81913716+Mikhaiel@users.noreply.github.com>
2021-08-20 16:42:48 +0530

    ❤️ J I M B R O O T A N - V 2.0 ❤️

--- plugins/happymod.js
+++ plugins/happymod.js
@@ -1,7 +1,9 @@
 /* Copyright (C) 2021 plk-dqz.
+
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-WhatsAsenaDuplicated*/
+WhatsAsenaDuplicated
+*/
 
 
 const Asena = require('../events');
